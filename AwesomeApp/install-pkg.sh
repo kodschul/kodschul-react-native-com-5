91d6c4a9371f77d3800235f50ed0a568d97fae5e
franznkemaka <info@franznkemaka.com>
2026-01-09 09:33:39 +0100

    🚀 install packages

--- AwesomeApp/install-pkg.sh
+++ AwesomeApp/install-pkg.sh
@@ -1,1 +1,6 @@
  yarn add react-native-ui-lib  @react-navigation/native  @react-navigation/stack  @react-native-masked-view/masked-view  @react-navigation/native-stack @react-navigation/bottom-tabs  react-native-reanimated  react-native-worklets react-native-screens react-native-safe-area-context @react-navigation/drawer @react-native-vector-icons/fontawesome6 @react-native-vector-icons/evil-icons react-native-vector-icons
+
+
+
+
+ yarn add react-native-notifications  react-native-mmkv react-native-nitro-modules react-native-maps
